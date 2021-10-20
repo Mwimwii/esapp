8cfe68e6953391bef89d3056085c4ae4f0d2cb67
shchiwamba <shchiwamba@yahoo.com>
2021-10-20 20:28:19 +0200

    Error and dead links free for Template, Cost Centre,Expense Category,Unit of Measure,Commodity Type, Funder,Component,Outcome,Output,Indicator,Activity, Commodity Specific - District, Commodity Specific - Provincial, Commodity Specific - PCO, Programme Wide – PCO and input.

--- backend/views/awpb-budget/updatepw.php
+++ backend/views/awpb-budget/updatepw.php
@@ -7,18 +7,27 @@ use yii\helpers\Html;
 
 $this->title = 'Update AWPB : ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'AWPB', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['viewpw', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="awpb-activity-line-update">
+<div class="card card-success card-outline">
+    <div class="card-body">
+<div class="awpb-activity-line-create">
+
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form_1', [
+    <?php
+ 
+   echo $this->render('_form_1', [
         'model' => $model,
          'template_id'=>$model->awpb_template_id,
         'status'=>$status
-            
-    ]) ?>
+    ]) ;
+    
 
+    // ($user->district_id > 0 || $user->district_id != '') 
+            ?>
+</div>
+</div>
 </div>
